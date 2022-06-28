76250edbe7d532a6dc03905496522de28e1544fd
lxr <2393901126>
2022-06-28 21:44:19 +0800

    Signed-off-by: lxr <2393901126>

---
+++ helloworld/app/src/main/java/com/example/lenovo/helloworld/helloworldo.kt
@@ -0,0 +1,12 @@
+package com.example.lenovo.helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class helloworldo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_helloworldo)
+    }
+}
